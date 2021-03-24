f08d2fefb1639ffa42c02dd9d5b4ccd415433e74
Maria Yammine <56266574+Maria-yammine@users.noreply.github.com>
2021-03-24 11:38:58 +0100

    Create AccelGyroMag.cpp

---
+++ Sources/AccelGyroMag.cpp
@@ -0,0 +1,12 @@
+/** @file AccelGyroMag.cpp
+ * 
+ *@brief
+ Read accelerometer, gyroscope and magnetometer values from
+inertial measurement unit: MPU9250 or LSM9DS1 over SPI on Raspberry Pi 
+and shares them with socket.
+ *
+ * @author Maria Estella Yammine      
+ * 
+ * @version 1.0
+ * 
+ */
